e80f215d30c1fe860721c07a9f43ac4cf89d6747
Chely Ho <adept_inept@yahoo.com>
2017-11-06 02:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = 'Mr. Blank';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = "Welcome,";
