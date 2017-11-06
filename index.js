44feca3a64da27c9f7cee0a6d75f1cd9d96f9958
Chely Ho <adept_inept@yahoo.com>
2017-11-06 01:58:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser ='mr. blank';
 
-const welcomeMessage = 'Welcome to Flatbook, '+ currentUser+'!';
+const welcomeMessage = 'Welcome to Flatbook, '+ currentUser +'!';
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser+'!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser +'!';
