4ecb6516dffd52ceb5f75e154e37bcacbab75e92
Chely Ho <adept_inept@yahoo.com>
2017-11-06 02:05:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser ='Mr. Blank';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
